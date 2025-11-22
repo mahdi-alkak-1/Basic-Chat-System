e83e947a983391f6553d5cdc59a6290487fbd587
mahdi-alkak1-1 <mahdialkak1@gmail.com>
2025-11-22 19:48:29 +0200

    Scripts: login & register

---
+++ Frontend/scripts/login.js
@@ -0,0 +1,36 @@
+async function login() {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const error = document.getElementById("error");
+
+    error.innerText = "";  // clear errors
+
+    if (!email || !password) {
+        error.innerText = "All fields required.";
+        return;
+    }
+
+    try {
+        const response = await axios.post("../backend/auth/login.php",
+            {   email: email,
+                password: password },
+            {
+                headers: { 'Content-Type': 'application/json'},
+            }
+        );
+
+        console.log(response.data); 
+
+        if (response.data.status === 200) {
+            localStorage.setItem("id", response.data.id)
+            localStorage.setItem("token", response.data.token);
+            window.location.href = "chat.html";
+        } else {
+            error.innerText = response.data.message;
+        }
+
+    } catch (err) {
+        error.innerText = "Server error.";
+        console.log(err);
+    }
+}
